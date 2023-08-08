56ded2be3dfe1e5795c30a23f3a0e923b97a6175
Isis Nilo <69352912+niloisis@users.noreply.github.com>
2023-08-08 11:53:31 -0300

    Add files via upload

---
+++ programming-logic/mod3-problem2.js
@@ -0,0 +1,14 @@
+/*
+Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+
+Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+*/
+
+// matriz de gatinhos: nome, tipo, sexo, idade
+gatinhos = [
+    ['Romeu', 'Siâmes', 'M', 2],
+    ['Lilo', 'Laranja', 'F', 8],
+    ['Tonho', 'Frajolinha', 'M', 5],
+    ['Lemos', 'Preto', 'M', 3],
+    ['Mimo', 'Branco', 'F', 7]
+]
