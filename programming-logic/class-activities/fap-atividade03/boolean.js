62fe544510546b48e4cd59e51b392a2531b563f3
Isis Nilo <69352912+niloisis@users.noreply.github.com>
2023-08-22 00:31:09 -0300

    class activities

---
+++ programming-logic/class-activities/fap-atividade03/boolean.js
@@ -0,0 +1,32 @@
+// Boolean 
+
+// 1. Crie uma variável chamada "estaChovendo" e atribua a ela o valor booleano que indica se está chovendo ou não. 
+
+var estaChovendo = true
+
+// 2. Declare uma variável chamada "verdadeiro" e atribua a ela o valor booleano true. 
+
+var verdadeiro = true
+
+// 3. Declare uma variável chamada "falso" e atribua a ela o valor booleano false. 
+
+var falso = false
+
+// 4. Verifique no terminal se a variável "verdadeiro" é igual a variável "falso". 
+
+console.log(verdadeiro == falso) // false
+
+// 5. Declare uma variável chamada "temperatura" e atribua a ela um valor numérico representando a temperatura atual. Em seguida, verifique e exiba no terminal se esta temperatura é maior que 30 graus Celsius. 
+
+var temperatura = 27
+console.log(temperatura > 30) // false
+
+// 6. Declare uma variável chamada "resultado" e atribua a ela o valor booleano resultante da comparação "10 > 5". Em seguida, exiba o valor da variável resultado no terminal. 
+
+var resultado = Boolean(10 > 5)
+console.log(resultado) // true
+
+// 7. Declare uma variável chamada "teste" e atribua a ela o valor booleano resultante da comparação "25 < 15". Em seguida, exiba o valor da variável resultado no terminal. 
+
+var teste = Boolean(25 < 15)
+console.log(teste) // false
---
+++ programming-logic/class-activities/fap-atividade03/string.js
@@ -0,0 +1,29 @@
+// STRING
+
+// 1. Crie uma variável chamada "nome" e atribua a ela uma string com seu nome. Em seguida, exiba o conteúdo da variável "nome" no terminal. 
+
+const nome = "Bob"
+console.log(nome)
+
+// 2. Crie uma variável chamada "frase" e atribua a ela uma frase de sua escolha. Em seguida, exiba o comprimento da string armazenada na variável "frase". 
+
+let frase1 = "bobo bobo amendo bobo yeah"
+console.log(frase1)
+
+// 3. Crie uma variável chamada "palavra" e atribua a ela uma palavra qualquer. Em seguida, exiba a primeira letra da palavra armazenada na variável "palavra". 
+
+const palavra = "bobo"
+console.log(palavra.charAt(0))
+
+// 4. Crie uma variável chamada "frase" e atribua a ela uma frase de sua escolha. Em seguida, exiba a frase em letras maiúsculas. 
+
+frase2 = "bobo bobo amendo bobo yeah"
+console.log(frase2.toUpperCase())
+
+// 5. Crie uma variável chamada "endereço" e atribua a ela um valor de sua escolha. Em seguida, sobrescreva o valor da variável para um outro endereço e verifique no terminal o valor atual da variável. 
+
+let endereco = "Rua dos Bobos, 0"
+console.log(endereco)
+
+endereco = "Fenda do Biquini"
+console.log(endereco)
